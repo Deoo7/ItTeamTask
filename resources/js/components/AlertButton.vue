3633a5bc538ef904525365d499fa4a9dc91a199d
Lyova Moroyan <Lyov.moroyan03@gmail.com>
2023-07-24 20:08:54 +0400

    Install Project

---
+++ resources/js/components/AlertButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to Inertia.js with Laravel 8 and Vue.js 2!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Laravel Vue 2 App</title>
+</head>
+<body>
+    @inertia
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
